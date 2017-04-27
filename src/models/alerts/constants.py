b7134f1f7d7a2626c01e11b15c539ad656733003
Sijoy Vijayan <sijoy.vijayan@gmail.com>
2017-04-27 12:20:20 +0530

    key unset

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,5 +1,5 @@
 URL = "https://api.mailgun.net/v3/sandboxb0914261c0254c6cad47023686fc73cf.mailgun.org/messages"
-API_KEY = "key-cd841a2dc9970dc83c34fd8d95eb2645"
+API_KEY = ""
 FROM = "Mailgun Sandbox <postmaster@sandboxb0914261c0254c6cad47023686fc73cf.mailgun.org>"
 ALERT_TIMEOUT = 1
 COLLECTION = "alerts"
