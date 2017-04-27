04c8becf12da84b8fbbfc0e991bc75952fe07423
Sijoy Vijayan <sijoy.vijayan@gmail.com>
2017-04-27 12:25:50 +0530

    environ variabls

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,6 +1,6 @@
-URL = "https://api.mailgun.net/v3/sandboxb0914261c0254c6cad47023686fc73cf.mailgun.org/messages"
-API_KEY = ""
-FROM = "Mailgun Sandbox <postmaster@sandboxb0914261c0254c6cad47023686fc73cf.mailgun.org>"
+URL = os.environ.get("MAILGUN_URL")
+API_KEY = os.environ.get("MAILGUN_API_KEY")
+FROM = os.environ.get("MAILGUN_FROM")
 ALERT_TIMEOUT = 1
 COLLECTION = "alerts"
 FQDN = "pricing.jstlvr.com"
