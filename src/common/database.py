82580f4e57c620870e555350d89aa52663007198
Sijoy Vijayan <sijoy.vijayan@gmail.com>
2017-05-07 19:30:50 +0530

    Updating URI and DB in database.py

--- src/common/database.py
+++ src/common/database.py
@@ -3,13 +3,13 @@ import os
 import pymongo
 
 class Database(object):
-    URI = os.environ.get("MONGOLAB_URI")
+    URI = "mongodb://127.0.0.1:27017"
     DATABASE = None
 
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client.get_default_database()
+        Database.DATABASE = client['fullstack']
 
     @staticmethod
     def insert(collection, data):
